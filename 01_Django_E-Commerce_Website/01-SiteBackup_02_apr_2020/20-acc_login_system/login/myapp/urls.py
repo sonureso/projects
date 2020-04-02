99f86742df12c973f0309fc09690d94ab40c0faf
sonureso <sonuresonance@gmail.com>
2020-04-02 05:11:17 -0700

    Delete urls.py

--- 01_Django_E-Commerce_Website/01-SiteBackup_02_apr_2020/20-acc_login_system/login/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-		path('',views.index,name="first_page"),
-		path('signup/',views.signup,name="Sign Up"),
-		path('login/',views.login,name="Login"),
-		path('logout/',views.logout,name="LogOut"),
-	]
