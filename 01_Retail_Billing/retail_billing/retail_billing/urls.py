93635312f5bdbde8af66c93bd2a79aeaa21a73b1
Shubham Kumar <sonuresonance@gmail.com>
2021-10-16 09:39:37 +0530

    Added info on ec2 deployment.

--- 01_Retail_Billing/retail_billing/retail_billing/urls.py
+++ 01_Retail_Billing/retail_billing/retail_billing/urls.py
@@ -1,8 +1,13 @@
 # main urls.py file:
 from django.contrib import admin
 from django.urls import path,include
+from django.views.static import serve
+from django.conf.urls import url
+from . import settings
+
 
 urlpatterns = [
     path('',include('products.urls')),
     path('admin/', admin.site.urls),
+	url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
