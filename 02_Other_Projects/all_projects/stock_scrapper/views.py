64614773ae68bda80fedb7ae44a0a93d6fd9830b
Shubham Kumar <sonuresonance@gmail.com>
2022-01-08 13:51:46 +0530

    feature: save files

--- 02_Other_Projects/all_projects/stock_scrapper/views.py
+++ 02_Other_Projects/all_projects/stock_scrapper/views.py
@@ -3,10 +3,20 @@ from datetime import datetime
 from urllib.request import Request, urlopen
 import json
 import pandas as pd
+import os
 
 def stock_home(request):
     data = {}
+    data['status'] = "not available"
     data['st_data'] = "nothing"
+    # save the list of available files:================
+    try:
+        path = "stock_scrapper/saved_data/"
+        data['files_list'] = os.listdir(path)
+        data['files_list'] = [{'name':file,'symbol':file.split('_')[0], 'tf':file.split('_')[1], 'st_date':file.split('_')[2], 'end_date':file.split('_')[3][:-4]} for file in data['files_list']]
+        #print("List of Files: ",data['files_list'])
+    except Exception as e:
+        print("Error getting saved files!")
     if request.method=='POST':
         d = {}
         d['symbol'] = request.POST['symbol']
@@ -17,9 +27,23 @@ def stock_home(request):
         d['end_date'] = d['end_date'][-2:]+"-"+d['end_date'][-5:-3]+"-"+d['end_date'][:4]
         d['st_date'] = get_t_stamp(d['st_date'])
         d['end_date'] = get_t_stamp(d['end_date'])
-        page = get_data(d['symbol'],d['st_date'],d['end_date'],d['tf'])
-        d = process_data(page)
+        d = get_data(d['symbol'],d['st_date'],d['end_date'],d['tf'])
+        data['url'] = d['url']
+        d = process_data(d['webpage'])
         data['st_data'] = d.to_dict('records')
+        # Save some data for next page:==================
+        data['status'] = "available"
+        data['symbol'] = request.POST['symbol']
+        data['tf'] = request.POST['tf']
+        data['st_date'] = request.POST['st_date']
+        data['end_date'] = request.POST['end_date']
+        if(request.POST['submit_button'] == ' Get Data '):
+            pass
+        else:
+            name = data['symbol']+"_"+data['tf']+"_"+data['st_date']+"_"+data['end_date']+".csv"
+            d.to_csv("stock_scrapper/saved_data/"+name,index=False)
+    else:
+        pass
     return render(request,'stocks_home.html',data)
 
 def get_data(symbol,st,et,r):
@@ -40,11 +64,14 @@ def get_t_stamp(date):
 
 def bring_data(url):
     try:
+        d = {}
+        d['url'] = url
         req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
         client = urlopen(req)
         webpage = client.read()
         client.close()
-        return webpage
+        d['webpage'] = webpage
+        return d
     except Exception as e:
         print("bring_data: Exception occurred:-",str(e))
         return 1
